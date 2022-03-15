fc75db3584f370a7a671873f07bf5098d2b08a7f
slavarak <94356617+slavarak@users.noreply.github.com>
2022-03-15 13:46:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+let title =prompt ("как называется ваш проект?", "Project");
+let screens = prompt ("Какие типы экранов нужног разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice= prompt ("Сколько будет стоить данная работа?", 12000);
+let rollback = Math.random() *150;
+let fullPrice = 100000;
+let adaptive = confirm("Нужен ли адаптив на сайте");
+
+
+
+let service1 = prompt ("Какой дополнительный тип услуг нужен?",service1)
+let servicePrice1 = prompt("Сколько это будет стоить?",servicePrice1 )
+let service2 =prompt("Какой дополнительный тип услуг нужен", service2)
+let servicePrise2 =prompt("Сколько это будет стоить?", servicePrise2)
+
+
+console.log ('title'+''+ title);
+console.log ('Стоимость разработки сайта'+''+fullPrice+''+'рублей');
+console.log ('adaptive'+''+adaptive);
+console.log ('Стоимость верстки экранов'+''+screenPrice+''+"рублей");
+console.log (screens.length);
+console.log (screens.toLowerCase);
+console.log (uTT);
+console.log('Процент отката посреднику за работу'+''+Vath.round(fukkPrice*(rollback/100)))
+
+console.log (typeof title);
+console.log (typeof screen);
+console.log (typeof screenPr);
+console.log (typeof rollback);
+console.log (typeof  fullPri);
+console.log (typeof adaptive);
+
+
+
+
+
+
